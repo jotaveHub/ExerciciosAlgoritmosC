9dfdbfda506a0578dd11d8508bd4982a3f0c89d4
jotaveHub <accurategx@gmail.com>
2025-10-23 15:24:35 -0300

    Adicionando ex10 da lista

--- Exercicios Lista/ex9.c
+++ Exercicios Lista/ex9.c
@@ -1,11 +1,24 @@
+/*Escreva  um  programa  que leia o valor  de  uma  distância  percorrida em  km  e  o  tempo 
+gasto em horas e calcule a velocidade média em m/s.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
+int VelocidadeMedia (int km, int tempo) {
+    int vm = km / tempo;
+    return vm;
+}
+
 int main() {
-    system("chcp 65001");
-    
-    
+    system("chcp 65001 > NUL");
+    int km,tempo;
+    printf("Informe a distância percorrida em KM:");
+    scanf("%d",&km);
+    printf("Informe o tempo gasto em horas:");
+    scanf("%d",&tempo);
+
+    printf("A velocidade média obtida foi: %dm/s.\n",VelocidadeMedia(km,tempo));
     system("pause");
     return 0;
 }
